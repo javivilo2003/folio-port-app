40d3c59b12bd37a20e7230d3cf916e992b70b727
javivilo2003 <jave1403@gmail.com>
2026-06-13 20:02:37 +0200

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,6 @@
 import type { Metadata } from "next"
-import {
-  Bricolage_Grotesque,
-  Instrument_Serif,
-  JetBrains_Mono,
-  Prata,
-} from "next/font/google"
 
 import "./globals.css"
-
-const bricolageGrotesque = Bricolage_Grotesque({
-  variable: "--font-bricolage-grotesque",
-  subsets: ["latin"],
-})
-
-const instrumentSerif = Instrument_Serif({
-  variable: "--font-instrument-serif",
-  subsets: ["latin"],
-  weight: ["400"],
-  style: ["normal", "italic"],
-})
-
-const jetBrainsMono = JetBrains_Mono({
-  variable: "--font-jetbrains-mono",
-  subsets: ["latin"],
-})
-
-const prata = Prata({
-  variable: "--font-prata",
-  subsets: ["latin"],
-  weight: ["400"],
-})
 
 export const metadata: Metadata = {
   title: "Javier Viloria",
@@ -42,10 +13,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${bricolageGrotesque.variable} ${instrumentSerif.variable} ${jetBrainsMono.variable} ${prata.variable} h-full antialiased`}
-    >
+    <html lang="en" className="h-full antialiased">
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   )
